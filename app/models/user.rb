240c202848417c02563bee649d4086e5c53c2292
daikiku10 <soccerdk19.910@gmail.com>
2020-11-05 18:55:53 +0900

    ログイン、ログアウト、新規登録の導線

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  with_options presence: true do
+    validates :shop_name
+    validates :instagram
+    validates :phone_number
+    validates :prefecture_id, numericality: { other_than: 1}
+    validates :city
+    validates :house_number
+  end
 end
