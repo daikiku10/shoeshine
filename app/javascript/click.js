f6145405c5ccdde5baa6436274b357fc79ef2d1f
daikiku10 <soccerdk19.910@gmail.com>
2020-11-09 16:38:38 +0900

    トップページの見た目の変更

--- app/javascript/click.js
+++ app/javascript/click.js
@@ -1,7 +1,11 @@
 function newMap (){
   const myShop = document.getElementById('select');
+  const entry = document.getElementById('entry');
+  const geocoder = new google.maps.Geocoder();
+
   myShop.addEventListener('click', function(){
-    myShop.innerHTML ="登録する"
+    myShop.innerHTML= "";
+    entry.innerHTML= "登録する";
 
     //東京の位置
     const options =  {
@@ -13,22 +17,38 @@ function newMap (){
     const map = new google.maps.Map(document.getElementById('map'), options);
 
     //クリックイベントを追加
-    map.addListener('click', function(e) {
-      getClickLatLng(e.latLng, map);
-    });
-    function getClickLatLng(lat_lng, map){
+    // map.addListener('click', function(e) {
+      // getClickLatLng(e.latLng, map);
 
-      document.getElementById('lat').textContent = lat_lng.lat();
-      document.getElementById('lng').textContent = lat_lng.lng();
-
-      const marker = new google.maps.Marker({
-        position: lat_lng,
-        map: map,
-        animation: google.maps.Animation.DROP
+    map.addEventListener('click', function(e) {
+      geocoder.geocode({location: e.latLng}, function(results, status) {
+        if(status === 'OK' && results[0]) {
+          map.panTo(results[0].geometry.location);
+          const marker = new google.maps.Marker({
+            position: results[0].geometry.location,
+            map: map,
+            animation: google.maps.Animation.DROP,
+          });
+        };
       });
-    };
+    });
+      
+      //マップ上にマーカーしてから登録をクリックする
+      entry.addEventListener('click', function(){
+        console.log("ok");
+      });
 
+    // function getClickLatLng(lat_lng, map){
 
+    //   document.getElementById('lat').textContent = lat_lng.lat();
+    //   document.getElementById('lng').textContent = lat_lng.lng();
+
+    //   // const marker = new google.maps.Marker({
+    //   //   position: lat_lng,
+    //   //   map: map,
+    //   //   animation: google.maps.Animation.DROP
+    //   });
+    // };
   });
 };
 
--- app/javascript/map.js
+++ app/javascript/map.js
@@ -1,7 +1,7 @@
 function initMap(){
   //東京の位置
   const options =  {
-    zoom:8,
+    zoom:6,
     center: {lat:35.681167, lng: 139.767052}
   };
   //google mapの表示
