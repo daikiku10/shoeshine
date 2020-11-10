bffa33a4f535e6a31f18fbdbfa57c0072829b423
daikiku10 <soccerdk19.910@gmail.com>
2020-11-10 15:59:14 +0900

    shopsテーブルへ保存

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -3,5 +3,16 @@ class ShopsController < ApplicationController
   end
 
   def new
+    @shop = Shop.new
+  end
+
+  def create
+    @shop = Shop.new(shop_params)
+    @shop.save
+  end
+
+  private
+  def shop_params
+    params.require(:shop).permit(:address, :phone_number, :instagram, :lat, :lng).merge(user_id: current_user.id)
   end
 end
