043218ee358d5584e53bc907353c730c73fbb68f
daikiku10 <soccerdk19.910@gmail.com>
2020-11-10 11:17:50 +0900

    shopのnewページに地図を表示

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,4 +1,7 @@
 class ShopsController < ApplicationController
   def index
   end
+
+  def new
+  end
 end
--- app/javascript/click.js
+++ app/javascript/click.js
@@ -1,42 +1,17 @@
 function newMap (){
-  const myShop = document.getElementById('select');
-  const entry = document.getElementById('entry');
-  const geocoder = new google.maps.Geocoder();
-
-  myShop.addEventListener('click', function(){
-    myShop.innerHTML= "";
-    entry.innerHTML= "登録する";
-
-    //東京の位置
-    const options =  {
-      zoom:15,
-      center: {lat:35.681167, lng: 139.767052}
-    };
-
-    //google mapの表示
-    const map = new google.maps.Map(document.getElementById('map'), options);
+  //東京の位置
+  const options =  {
+    zoom:15,
+    center: {lat:35.681167, lng: 139.767052}
+  };
+  //google mapの表示
+  const map = new google.maps.Map(document.getElementById('new-map'), options);
 
     //クリックイベントを追加
     // map.addListener('click', function(e) {
       // getClickLatLng(e.latLng, map);
 
-    map.addEventListener('click', function(e) {
-      geocoder.geocode({location: e.latLng}, function(results, status) {
-        if(status === 'OK' && results[0]) {
-          map.panTo(results[0].geometry.location);
-          const marker = new google.maps.Marker({
-            position: results[0].geometry.location,
-            map: map,
-            animation: google.maps.Animation.DROP,
-          });
-        };
-      });
-    });
       
-      //マップ上にマーカーしてから登録をクリックする
-      entry.addEventListener('click', function(){
-        console.log("ok");
-      });
 
     // function getClickLatLng(lat_lng, map){
 
@@ -49,7 +24,6 @@ function newMap (){
     //   //   animation: google.maps.Animation.DROP
     //   });
     // };
-  });
 };
 
 window.addEventListener("load", newMap);
