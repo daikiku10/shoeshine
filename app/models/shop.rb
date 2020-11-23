8334573ab7e3707f90267e68d579f1c6bd5d74bf
daikiku10 <soccerdk19.910@gmail.com>
2020-11-23 14:36:36 +0900

    Lat/Lngのエラーメッセージ変更

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,12 +1,17 @@
 class Shop < ApplicationRecord
   belongs_to :user
+  after_validation :remove_unnecessary_error_messages
 
   with_options presence: true do
     validates :address, length: {maximum: 40}
     validates :instagram, length: {maximum: 30}
     validates :phone_number, format: {with: /\A\d{10}$|^\d{11}\z/}
-    validates :lat
+    validates :lat, presence: { message: '指定場所を地図にクリックしてください' }
     validates :lng
+  end
+
+  def remove_unnecessary_error_messages
+    errors.messages.delete(:lng)
   end
 
 end
